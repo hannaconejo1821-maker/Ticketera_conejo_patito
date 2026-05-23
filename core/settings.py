64813e1cf70a64a5edeacaef1a2f8ab7daaa5359
Hanna-art543 <amirandalo@unsa.edu.pe>
2026-05-23 18:30:21 -0500

    Fix: agregar allowed hosts para Render

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=^=odab##^$%u^4)ui(t%tvvo(6$l&%tq&a!ghs%nq!9!rb(u(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ticketera-conejo-patito.onrender.com', 'localhost', '127.0.0.1', '*']
 
 
 # Application definition
