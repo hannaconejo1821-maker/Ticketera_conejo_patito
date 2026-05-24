d736f57a495878b1549abd29b9ff541f3aa43d02
Hanna-art543 <amirandalo@unsa.edu.pe>
2026-05-23 19:19:16 -0500

    Permitiendo y subiendo carpeta media con imagenes

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('cupones.urls')),
-]
-
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = [
+    path('admin/', admin.site.split),
+    path('', include('cupones.urls')), 
+]
+
+# ¡ESTA ES LA LÍNEA MÁGICA!
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Esto ayuda a que Render pueda servir los archivos subidos directamente de forma temporal
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
